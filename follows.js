2e61c45d8f52fcc05b6befa52bec7ceb7368d0a2
mva72 <micha.vanaken@gmail.com>
2014-07-21 16:04:44 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,8 +12,8 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
+	var defaultKey		= 'B9DBJvfuS3UDChzvDRBjMyAqlqGcbzHfhvS4GSVq5fr3EAHn', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1071745745], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
