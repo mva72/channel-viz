d996202684d8f5a2cd8f115cc11c270b31b344d7
mva72 <micha.vanaken@gmail.com>
2014-07-21 20:50:03 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'B9DBJvfuS3UDChzvDRBjMyAqlqGcbzHfhvS4GSVq5fr3EAHn', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1071745745], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+		applicationName	= 'MvA test site', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
