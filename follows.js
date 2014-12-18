97e94346f790673fb03170f392917a88d7a01ad6
mva72 <micha.vanaken@gmail.com>
2014-12-18 15:49:12 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -176,7 +176,7 @@
 									xAxis.render();
 
 									// Define and Render Y Axis (Datastream Values)
-									var yAxis = new Rickshaw.Graph.Axis.Y.scaled( {
+									var yAxis = new Rickshaw.Graph.Axis.Y( {
 										graph: graph,
 										//scale: scale
 										tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
